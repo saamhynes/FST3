9c5f302b384a1825e88755d2776f009c9b0c6505
prawstho <peter.rawsthorne@keyin.com>
2022-12-07 20:52:37 -0330

    passport local strategy with mongodb and postgres

---
+++ server.js
@@ -0,0 +1,109 @@
+if( process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const uuid = require('uuid')
+const logins = require('./services/p.logins.dal') // use POSTGRESQL dal
+//const logins = require('./services/m.logins.dal') // use MONGODB dal
+const app = express();
+global.DEBUG = false;
+passport.use(new localStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    let user = await logins.getLoginByEmail(email);
+    if( user == null ) {
+        return done(null, false, {message: 'No user with that email.'})
+    }
+    try {
+        if( await bcrypt.compare(password, user.password)) {
+            return done(null, user); 
+        } else {
+            return done(null, false, {message: 'Incorrect password was entered.'});
+        }
+    } catch (error) {
+        return done(error);
+    }
+}))
+passport.serializeUser((user, done) => {
+    done(null, user._id)
+});
+passport.deserializeUser( async (id, done) => {
+    let user = await logins.getLoginById(id);
+    if(DEBUG) console.log('passport.deserializeUser: ' + user);
+    done(null, user);
+});
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.username });
+});
+
+app.get('/sample1', checkAuthenticated, (req, res) => {
+    res.render('sample.one.ejs');
+});
+app.get('/sample2', checkAuthenticated, (req, res) => {
+    res.render('sample.two.ejs');
+});
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        let result = await logins.addLogin(req.body.name, req.body.email, hashedPassword, uuid.v4());
+        res.redirect('/login');
+    } catch(error) {
+        console.log(error);
+        res.redirect('/register');
+    }
+});
+
+app.delete('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/login');
+    });
+  });
+
+function checkAuthenticated(req, res, next) {
+    if ( req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if ( req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+    return next();
+}
+
+app.listen(3000);
---
+++ services/m.logins.dal.js
@@ -0,0 +1,50 @@
+const dal = require("./m.db");
+
+async function getLogins() {
+  try {
+    await dal.connect();
+    const cursor = await dal.db("OceanSet").collection("logins").find();
+    const results = await cursor.toArray();
+    return results;
+  } catch(error) {
+    console.log(error);
+  }
+};
+async function getLoginByEmail(email) {
+  try {
+    await dal.connect();
+    const result = await dal.db("OceanSet").collection("logins").findOne({ "email": email });
+    if(DEBUG) console.error('mlogins.getLoginByEmail(' + email + '): ' + result)
+    return result;
+  } catch(error) {
+    console.log(error);
+  }
+};
+async function getLoginById(id) {
+  try {
+    await dal.connect();
+    const result = await dal.db("OceanSet").collection("logins").find({ "_id": id });
+    if(DEBUG) console.error('mlogins.getLoginById(' + id + '): ' + result)
+    return result;
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+async function addLogin(name, email, password, uuidv4) {
+  let newLogin = JSON.parse(`{ "username": "` + name + `", "email": "` + email + `", "password": "` + password + `", "uuid": "` + uuidv4 + `" }`);
+  try {
+    await dal.connect();
+    const result = await dal.db("OceanSet").collection("logins").insertOne(newLogin);
+    return result.insertedId;
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+    getLogins,
+    addLogin,
+    getLoginByEmail, 
+    getLoginById,
+  }
